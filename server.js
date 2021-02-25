c78376ccc20c5acaef48aa0e9a02b66cab7b27a9
Thomas James <thomas.james@sainsburys.co.uk>
2021-02-25 20:27:45 +0530

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri= process.env.ATLAS_URI;
-mongoose.connect("mongodb+srv://hexlrtech:112233.ad@cluster0.3e8rn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
+mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
 
 const connection = mongoose.connection;
 
@@ -19,9 +19,9 @@ connection.once('open',()=>{
     console.log("Mongo db running");
 })
 
-const supportRouter =require('./routes/support');
+const supportRouter =require('./src/routes/support');
 app.use('/support',supportRouter)
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
-});
+}); 
