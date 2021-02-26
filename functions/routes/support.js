b7a52adc98089ade99df9a17e5fb10d50f8b6ec8
Thomas James <thomas.james@sainsburys.co.uk>
2021-02-27 00:25:10 +0530

    added mail box

--- functions/routes/support.js
+++ functions/routes/support.js
@@ -1,5 +1,15 @@
 const router=require('express').Router();
 let Support = require('../models/support.model');
+var nodemailer = require ('nodemailer');    
+
+var transporter = nodemailer.createTransport ({ 
+    service: 'gmail', 
+    auth: { 
+            user: 'thomasjames.adg@gmail.com', 
+            pass: '112233.Ad' 
+        } 
+    });
+
 
 router.route('/').get((req,res)=>{
     Support.find()
@@ -20,7 +30,28 @@ router.route('/add').post((req,res)=>{
      }
      const newSupport = new Support(supportDetails);
      newSupport.save()
-     .then((res1)=> res.json(res1._id))
+     .then((res1)=> {
+        const mailOptions = { 
+            from: 'thomasjames.adg@gmail.com',  
+            to: 'tonyjose420@gmail.com,antonythomas96.96@gmail.com,mevinxavier000@gmail.com,mail@thomasjames.in',  
+            subject: `We have a New request Please take Action for `+req.body.name+``, 
+            html: `
+            <pre>Name:`+req.body.name+`,
+            Company: `+req.body.comapny+`,
+            Phone: `+req.body.phone+`,
+            Email: `+req.body.email+`,
+            Reason: `+req.body.reason+`,
+            Description: `+req.body.description+`
+            </pre>` 
+          };
+          transporter.sendMail (mailOptions, function (err, info) { 
+            if (err) 
+              console.log (err) 
+            else 
+              console.log (info); 
+          })
+         
+        res.json(res1._id)})
      .catch(err => res.status(400).json('Error'+err))
 
 })
