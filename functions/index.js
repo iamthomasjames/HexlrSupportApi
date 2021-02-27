4979b04dd4e8f79f16a6876b6899995ed271bf1f
Thomas James <thomas.james@sainsburys.co.uk>
2021-02-27 13:15:14 +0530

    bug fixed

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,24 @@ require('dotenv').config();
 const PORT = 4000;
 app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://trusting-dubinsky-942dd3.netlify.app/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const uri= process.env.ATLAS_URI;
 mongoose.connect(process.env.ATLAS_URI,{useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
