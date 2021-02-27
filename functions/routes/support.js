93862237eb16291636a310e4ff48fc86c6c2d309
Thomas James <thomas.james@sainsburys.co.uk>
2021-02-27 13:05:58 +0530

    changes

--- functions/routes/support.js
+++ functions/routes/support.js
@@ -55,11 +55,11 @@ router.route('/add').post((req,res)=>{
           transporter.sendMail (mailOptions, function (err, info) { 
             if (err) {
                 console.log (err) 
-                res.json(res1._id+err)
+                res.json(res1._id+JSON.stringify(err))
             }      
             else {
                 console.log (info); 
-                res.json(res1._id+info)
+                res.json(res1._id+JSON.stringify(info))
             }
              
           })
